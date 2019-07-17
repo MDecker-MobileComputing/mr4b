f86998bd30ae9be1f9a105be6aea184aacf4d923
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-07-17 21:02:52 +0200

    build-manpage: chmod u+x and improved comments in coding

--- manpage/build-manpage.sh
+++ manpage/build-manpage.sh
@@ -3,9 +3,12 @@
 TARGET_FOLDER=output
 
 mkdir -p $TARGET_FOLDER
+# -p: Create folder if needed, but no error if it already exists
+
 
 gzip -c mr4b.1 > ${TARGET_FOLDER}/mr4b.1.gz
 # -c: Do not alter input file, write compressed output to STDOUT
+
 
 echo -e "\nMan page was created: "$(find output -type f)
 echo
